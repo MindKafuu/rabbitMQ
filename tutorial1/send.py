41ab2321e65185016d220879c688420636c6b995
Mind Kafuu <chanagan_mind@msn.com>
2022-08-19 14:33:02 +0700

    hidden url

--- tutorial1/send.py
+++ tutorial1/send.py
@@ -1,6 +1,6 @@
 import pika
 
-parameters = pika.URLParameters('amqp://guest:guest@10.131.200.163:5672/')
+parameters = pika.URLParameters('URL')
 connection = pika.BlockingConnection(parameters=parameters)
 channel = connection.channel()
 
